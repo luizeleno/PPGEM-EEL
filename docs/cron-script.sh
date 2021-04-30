d7c821f3fca705a4bc5dfa8b6bca08d2586d8f83
Luiz T. F. Eleno <luizeleno@gmail.com>
2021-04-30 14:00:49 -0300

    Build site at 2021-04-30 17:00:49 UTC

--- docs/cron-script.sh
+++ docs/cron-script.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/bash
 
-date
+echo '#' `date`
 
 echo "Sincronizando com o Google Drive..."
 cd /home/eleno/Dropbox/Lorena/Website/PPGEM-EEL/_Drive
 rclone sync edrf:PPGEM-site .
 
-echo "Atualizando o site no Github..."
+echo "# Atualizando o site no Github..."
 cd ..
 rake build
 rake commit
